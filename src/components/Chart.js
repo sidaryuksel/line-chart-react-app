22d955a8baed0045ae9e680b5db84604eec8135f
sidaryuksel <sidartastan@gmail.com>
2021-06-16 22:53:10 +0300

    warning cleared

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, LabelList, ResponsiveContainer } from 'recharts';
 
 const lineChartReport = [
